16ad1e7424d7d82a8f4e32f7aa1e162d0e553df0
JTushar <tushar@heady.io>
2023-11-24 21:16:57 +0530

    Initial Commit

---
+++ SwiftUIMap+MVVM/SwiftUIMap_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMap_MVVMApp.swift
+//  SwiftUIMap+MVVM
+//
+//  Created by Tushar Jaunjalkar on 24/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMap_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
